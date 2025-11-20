81491f424ed6358d59bb8268bf4170e461a77e8c
mackstm <IsmaelJos@github.com>
2025-11-20 17:50:46 +0000

    feat:

--- splitexpenser-back/main.py
+++ splitexpenser-back/main.py
@@ -365,4 +365,4 @@ api.add_namespace(expenses_ns, path="/groups")
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8003)
