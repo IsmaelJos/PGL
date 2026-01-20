2f340c3a795777edef3e1cb342397e6ffb34a1ca
mackstm <IsmaelJos@github.com>
2026-01-20 16:19:25 +0000

    feat:

--- splitexpenser-front-clase/app/groupdetail.tsx
+++ splitexpenser-front-clase/app/groupdetail.tsx
@@ -1,13 +1,29 @@
 import { useRouter } from "expo-router";
 import React, { useContext, useState } from "react";
-import { Button, Text, TextInput, View, Alert } from "react-native";
+import { Button, Text, TextInput, View, Alert, Modal } from "react-native";
 import { AuthContext } from "../context/AuthContext";
 
 export default function Login() {
   const { login, token } = useContext(AuthContext);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [newAmount, setNewAmount] = useState(0);
+  const [newDescription, setNewDescription] = useState("");
+  const [currentExpenseId, setcurrentExpenseId] = useState("");
   const [expenses, setExpenses] = useState([
     {
       id: "e.id",
+      desc: "e.description",
+      amount: "e.amount",
+      paid_by: "e.paid_by",
+    },
+    {
+      id: "e.id2",
+      desc: "e.description",
+      amount: "e.amount",
+      paid_by: "e.paid_by",
+    },
+    {
+      id: "e.id3",
       desc: "e.description",
       amount: "e.amount",
       paid_by: "e.paid_by",
@@ -27,22 +43,33 @@ export default function Login() {
     }
   };
 
-  const handleEdit = () => {
-    Alert.alert("Voy a edit");
+  const handleEdit = (id:string) => {
+    setModalVisible(false);
+    Alert.alert(`voy a editar el gasto ${currentExpenseId} con cantidad ${newAmount} y descripcion ${newDescription}`)
   };
+
+  const openModal = (id : string) => {
+    setcurrentExpenseId(id);
+    setModalVisible(true);
+  }
 
   return (
     <View style={{ flex: 1, justifyContent: "center", padding: 20 }}>
       <Text>Mi grupo</Text>
       {expenses.map((e) => (
         <View key={e.id}>
-          <Text
-            onPress={() => router.replace("/groupdetail")}
-          >{`Precio: ${e.amount} - ${e.desc}`}</Text>
-          <Button title="Editar" onPress={handleEdit} />
+          <Text>{`${e.amount} - ${e.desc}`}</Text>
+          <Button title="Modificar" onPress={() => openModal(e.id)} />
           <Button title="Borrar" onPress={() => handleDelete(e.id)} />
         </View>
       ))}
+
+      <Modal visible={modalVisible}>
+        <Text>Editando expense #{currentExpenseId}</Text>
+        <TextInput value={newAmount} onChange={(text)=>setNewAmount(text)}></TextInput>
+        <TextInput value={newDescription} onChange={(text)=>setNewDescription(text)}></TextInput>
+        <Button title=""></Button>
+      </Modal>
 
       <Button title="Volver a mis grupos" onPress={() => router.replace("/")} />
     </View>
---
+++ splitexpenser-front-clase/app/groupdetailOLD.tsx
@@ -0,0 +1,71 @@
+import { useRouter } from "expo-router";
+import React, { useContext, useState } from "react";
+import { Button, Text, TextInput, View, Alert } from "react-native";
+import { AuthContext } from "../context/AuthContext";
+
+export default function Login() {
+  const { login, token } = useContext(AuthContext);
+  const [expenses, setExpenses] = useState([
+    {
+      id: "e.id",
+      desc: "e.description",
+      amount: "e.amount",
+      paid_by: "e.paid_by",
+    },
+    {
+      id: "e.id2",
+      desc: "e.description",
+      amount: "e.amount",
+      paid_by: "e.paid_by",
+    },
+    {
+      id: "e.id3",
+      desc: "e.description",
+      amount: "e.amount",
+      paid_by: "e.paid_by",
+    },
+  ]);
+  const router = useRouter();
+
+  const handleDelete = (id: string) => {
+    console.log("Borro el id "+ id);
+    Alert.alert("Voy a borrar el id "+ id);
+    for (let i = 0; i < expenses.length; i++) {
+      console.log(expenses[i]);
+      if(expenses[i].id == id){
+        expenses.splice(i,1);
+        setExpenses([...expenses]);
+      }
+    }
+  };
+
+  const handleEdit = (id:string) => {
+    for (let i = 0; i < expenses.length; i++) {
+      const element = expenses[i];
+      if (element.id == id) {
+        Alert.alert(`${element.amount} - ${element.desc}`)
+      }
+    }
+    Alert.alert("Voy a edit");
+  };
+
+  const handleChange = (index: number, field:string, text:string)=>{
+    
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", padding: 20 }}>
+      <Text>Mi grupo</Text>
+      {expenses.map((e) => (
+        <View key={e.id}>
+          <TextInput keyboardType="number-pad" onChangeText={(text)=>(handleChange(index, "field", text))}>{e.amount}</TextInput>
+          <TextInput >{e.desc}</TextInput>
+          <Button title="Actualizar" onPress={() => handleEdit(e.id)} />
+          <Button title="Borrar" onPress={() => handleDelete(e.id)} />
+        </View>
+      ))}
+
+      <Button title="Volver a mis grupos" onPress={() => router.replace("/")} />
+    </View>
+  );
+}
